11e5ac408623ef60b19a12f83074eb1e2c9a7abb
Lorraine Figueroa <nyguerrillagirl@brainycode.com>
2022-01-28 16:39:04 -0500

    Adding the view templates

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Getting MEAN with Mongo, Express and Angular
 
-This repository holds the various chapter projects from following
-this book.
-master - chapter 3 - initial static project
+## Branch: GETTING_MEAN_CHAPTER_04
+
+This chapter covers:
+* Prototyping an application by building a static version
+* Defining routes for application URLs
+* Creating views in Express by using Pug and Bootstrap
+* Using controllers in Express to tie routes to views
+* Passing data from controllers to views
 
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,12 +1,20 @@
-var express = require('express');
-var router = express.Router();
-const crtlMain = require('../controllers/main');
+const express = require('express');
+const router = express.Router();
+const ctrlLocations = require('../controllers/locations');
+const ctrlOthers = require('../controllers/others');
+//const ctrlMain = require('../controllers/main');
 
-const homepageController = (req, res) => {
-  res.render('index', { title: 'Express'});
-};
+// const homepageController = (req, res) => {
+//   res.render('index', { title: 'Express'});
+// };
 
-/* GET home page. */
-router.get('/', crtlMain.index);
+/* Location pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* Other pages */
+router.get('/about', ctrlOthers.about);
+
 
 module.exports = router;
